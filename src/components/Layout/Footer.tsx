6a3565f12159e18594f17889de868a5cf1a3ee8a
Jonas Frießlich <jonas.friesslich@fmc-ag.com>
2023-11-26 16:12:43 +0100

    fix: github logo mobile

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,18 +1,18 @@
-import Link from "next/link";
-import Image from "next/image";
 import githubLogo from "../../assets/github-icon.svg";
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <section>
       <footer className="w-full py-6 px-4 border-t border-border-grey dark:border-white/10 flex flex-col md:flex-row justify-between">
-        <div className="flex items-center mx-4 mb-4 md:mb-0">
-          <Image src={githubLogo} width={50} className="" />
-          <p className="text-center font-medium text-base leading-[16px] mx-4 mb-4 md:mb-0">
+        <div className="flex flex-row items-center justify-center mb-4 md:mb-0">
+          <Image src={githubLogo} width={50} alt="GitHub Logo" />
+          <p className="font-medium text-base leading-[16px] mx-4">
             Maintained by <Link href="https://github.com/fugashu">Fugashu</Link>{" "}
             & <Link href="https://github.com/msMatix">msMatix</Link>
           </p>
         </div>
-
         <p className="text-center font-medium text-base leading-[16px] mb-4 md:mb-0">
           <Link href="/disclosure-statement">
             <a className="inline-block mx-2">Disclosure statement</a>
