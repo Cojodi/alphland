351de223e86ad7a8b5c321c4cabf5966f7b4df87
YYBer <liyuanying225@gmail.com>
2026-05-07 11:06:16 +0200

    refactor: grant logo

--- src/data/categories.tsx
+++ src/data/categories.tsx
@@ -25,6 +25,7 @@ import profileLight from "../assets/icons/profileLight.svg";
 import quests from "../assets/icons/quests.svg";
 import questsLight from "../assets/icons/questsLight.svg";
 import star from "../assets/icons/starFilled.svg";
+import starEmpty from "../assets/icons/starEmpty.svg";
 import swap from "../assets/icons/swap.svg";
 import swapLight from "../assets/icons/swapLight.svg";
 import time from "../assets/icons/time.svg";
@@ -81,8 +82,8 @@ export const categories = [
   {
     key: "grant_recipient",
     name: "Grant Recipient",
-    icon: star,
-    iconDark: star,
+    icon: starEmpty,
+    iconDark: starEmpty,
   },
 ];
 
